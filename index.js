ad07ba90d49f4ff68a9c15486d3240289a4962e4
unknown <sinan956733@gmail.com>
2026-05-05 12:07:55 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Welcome to the Home Page!");
+for(let i = 1; i <= 5; i++) {
+    let row= ''
+    for(let j = 1; j <= i; j++) {
+        row+='*'
+        
+    }
+    console.log(row)
+    
+        
+      
+}
